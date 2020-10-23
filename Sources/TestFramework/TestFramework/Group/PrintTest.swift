bd171c39efa29a834ec206ac00aadb7f92786a6b
kangtaewoo <>
2020-10-23 12:42:08 +0900

    dd

---
+++ Sources/TestFramework/TestFramework/Group/PrintTest.swift
@@ -0,0 +1,16 @@
+//
+//  PrintTest.swift
+//  TestFramework
+//
+//  Created by KangTW on 2020/10/23.
+//
+
+import Foundation
+
+open class PrintTest: NSObject {
+    
+    public static func printText(text: String) {
+        print(text)
+    }
+    
+}
